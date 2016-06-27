afb1cfa4b3439fbf45864c05e31b98eb7f2a7956
Pratul21 <pratul.t@practo.com>
2016-06-27 16:39:01 +0530

    Added crud

---
+++ crud/src/CustomBookBundle/Form/BookType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CustomBookBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('author')
+            ->add('language')
+            ->add('publisher')
+            ->add('summary')
+            /*->add('year')*/
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CustomBookBundle\Entity\Book'
+        ));
+    }
+}
